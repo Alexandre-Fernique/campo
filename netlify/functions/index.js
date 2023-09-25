4098a5b3c64b5410097e75cd212bf6d6e4d6cad7
Alexandre FERNIQUE <alexandre@fernique.fr>
2023-09-25 21:08:39 +0200

    remove \\ quote

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -18,7 +18,7 @@ initializeApp({
   credential: cert({
     projectId: process.env.PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/gm, "\n")
   }),
 })
 const db = getFirestore();
